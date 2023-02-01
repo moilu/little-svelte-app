b15ee7046795fb6d17291e884a458a50acb6a644
Moises Luna <moises.luna@srax.com>
2023-01-31 17:17:31 -0800

    Repository init

---
+++ src/components/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let count: number = 0;
+    $: doubled = count * 2;
+    $: quadrupled = doubled * 2;
+</script>
+
+<p>Count: {count}</p>
+<p>Doubled: {doubled}</p>
+<p>Quadrupled: {quadrupled}</p>
+<button on:click={()=>{ count++ }}>Increase Count</button>
